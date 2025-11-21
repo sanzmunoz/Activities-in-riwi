33ca91584bd2edd61026a61dce4c6f743d3ed5ef
santiagozmunoz <san.mu.zap@gmail.com>
2025-11-21 13:20:21 -0500

    cambios

--- Historia_de_Usuario/app.py
+++ Historia_de_Usuario/app.py
@@ -1,36 +1,36 @@
-import servicios
+from servicios import agregar_producto
 print("-"*5,"Menu para inventarios","-"*5)
 
-salida=0
-while salida!=7:
-    print("OPCIONES:" \
-    "1 - Agregar Producto" \
-    "2 - Mostrar Inventario" \
-    "3 - Buscar Producto" \
-    "4 - Atualizar Producto" \
-    "5 - Eliminar Producto" \
-    "6 - Calcular Estadisticas" \
+inventarios = []
+
+while True:
+    print("OPCIONES:\n" \
+    "1 - Agregar Producto\n" \
+    "2 - Mostrar Inventario\n" \
+    "3 - Buscar Producto\n" \
+    "4 - Actualizar Producto\n" \
+    "5 - Eliminar Producto\n" \
+    "6 - Calcular Estadisticas\n" \
     "7 - SALIR")
 
-    salida=int(input("Ingrese la opcion => "))
-    if salida == 1:
-        servicios.agregar_producto()
+    salida=input("Ingrese la opción => \n")
 
-    elif salida == 2:
-        servicios.muestra_inventarios()
+    if salida == "1":
+        New_list=agregar_producto()
+        inventarios.extend(New_list)
 
-    elif salida == 3:
-        servicios.buscar_producto()
-
-    elif salida == 4:
-        servicios.actualizar_producto()
-
-    elif salida == 5:
-        servicios.eliminar_producto()
-
-    elif salida == 6:
-        servicios.inventario_estadisticas()
-
-    elif salida == 7:
+    # elif salida == "2":
+    #     muestra_inventarios(inventarios)
+    # elif salida == "3":
+    #     buscar_producto(inventarios)
+    # elif salida == "4":
+    #     actualizar_producto(inventarios)
+    # elif salida == "5":
+    #     eliminar_producto(inventarios)
+    # elif salida == "6":
+    #     inventario_estadisticas(inventarios)
+    elif salida == "7":
         print("Saliendo...")
         break
+    else:
+        print("Opcion incorrecta")
--- Historia_de_Usuario/servicios.py
+++ Historia_de_Usuario/servicios.py
@@ -1,42 +1,60 @@
-inventarios = []
+def agregar_producto(): 
+    lista=[]
+    x=1
+    print("fincion 1")
+    while x!=0:
+        produ=input("Nombre de producto => ")
+        while True:
+            try:
+                cost=float(input("Cual es el precio del producto => "))
+                if cost < 0:
+                    print("Número erroneo")
+                else:
+                    break
+            except ValueError: 
+                print("Datos erroneos, ingrese de nuevo => ")
 
-def agregar_producto(inventarios, produ, cost, cant): #Funcion para agregar productos
-    #inventarios = []
+        while True:
+            try:
+                cant=int(input("Ingrese La cantidad del producto => "))
+                if cant <0:
+                    print("Número erroneo")
+                else:
+                    break
+            except ValueError:
+                print("Datos erroneos, ingrese de nuevo => ")
 
-    while True:
-        try:
-            produ=input("Nombre de producto => ") #pide al usuario el nombre del producto
-            cost=float(input("Cual es el precio del producto => ")) #Solicita ingreso de la cantidad del producto
-            cant=int(input("Ingrese La cantidad del producto => "))
-            break #usado para terminar el bucle 
-        except ValueError: #Captura el error
-            print("Datos erroneos, ingrese de nuevo => ")
+        producto={"nombre":produ, "precio":cost, "cantidad":cant}
+        lista.append(producto)
+        x=input("agregar otro producto s/n? ").lower()
+        if x == "s":
+            continue
+        else:
+            print("Entro")
+            x=0
+    return lista #Se agregan a una lista para utilizar despues
 
-    producto={"nombre":produ, "precio":cost, "cantidad":cant}
-    inventarios.append(producto)
-    return inventarios #Se agregan a una lista para utilizar despues
-
-def muestra_inventarios(): #Funcion para agregar inventarios
+def muestra_inventarios(inventarios): #Funcion para agregar inventarios
     if not inventarios:  #verifica si el inventario esta vacio y muestra mensaje
             print("inventario vacio")
     
     for inventario in inventarios: #Muestra los productos almacenados
             print(f"Producto: {inventario["Nombre"]}|Precio: {inventario["Precio"]}|Cantidad: {inventario["Cantidad"]}")
 
-def buscar_producto():
+def buscar_producto(inventarios):
     busqueda=input("¿Que producto desea buscar? \n ")
     for item in inventarios:
         if busqueda == item:
             print("El producto existe")
 
-def actualizar_producto():
+def actualizar_producto(inventarios):
     print("Vas a actualizar un producto")
     
 
-def eliminar_producto():
+def eliminar_producto(inventarios):
     print("Vas a liminar un producto") 
 
-def inventario_estadisticas(): #Funcion para mostrar estadisticas basicas de los productos ingresados3
+def inventario_estadisticas(inventarios): #Funcion para mostrar estadisticas basicas de los productos ingresados3
     suma=0 #creacion de variable para mostrar total de operacion
     regist=0 #variable para iterar en n productos
     for inventario in inventarios: #bucle para iterar en los productos existentes
