60681b715c9ed5e6b479a47b56a045633efe1f01
sanzmunoz <san.mu.zap@gmail.com>
2026-01-18 13:09:03 -0500

    cambios en week1

--- Modulo_3/week1/exercise/script1.js
+++ Modulo_3/week1/exercise/script1.js
@@ -1,10 +1,41 @@
 let task =[];
 let num = 0;
+const list = document.getElementById("taskList")
 
-const increment =()=>{
-    num++
+function increment(){
+    num++;
     console.log(`tarea N°${num}`);
-    contador.innerHTML = `<h2>¡Agregaste la tarea N°${num}!</h2>;`;
-    task.push(title)
-    console.log();
+    contador.innerHTML = `<h2>¡Agregaste la tarea N°${num}!</h2>`;
+}
+
+const miboton = document.getElementById("addButon");
+miboton.addEventListener('click', ()=> {
+    const newTask = input.value.trim(); //quitamos espacios
+    if (newTask !== ""){
+        increment(); //actualiza el contador
+        task.push(newTask); //Se agrega al arreglo
+        input.value = ""; //se limpia el input
+        renderTasks(); // Se muestra la lista actualizada
+    }
+});
+
+let contador = document.getElementById("contador")
+let tittle;
+let input = document.getElementById("myInput");
+input.addEventListener("input", (event) => {
+    console.log("el texto es: ", event.target.value);
+    tittle = event.target.value    
+}); 
+
+// funcion que muestra las tareas agregadas
+function renderTasks() {
+    //limpieza de lista
+    list.innerHTML = "";
+
+    //recorrer el arreglo y crear etiqueta <li> por tarea
+    task.forEach(function(item) {
+        const li = document.createElement("li");
+        li.textContent = item;
+        list.appendChild(li);
+    });
 }
