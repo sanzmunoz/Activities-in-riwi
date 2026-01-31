ea1394d000ae164021f21c862c7f72b288ca2ee1
sanzmunoz <san.mu.zap@gmail.com>
2026-01-31 15:05:56 -0500

    ultimo cambio

--- Modulo_3/week1/exercise/scrpt2.js
+++ Modulo_3/week1/exercise/scrpt2.js
@@ -1,9 +1,9 @@
 
-// 1. Seleccion de elementos porel ID
+// Seleccion de elementos porel ID
 const boton = document.getElementById("change");
 const texto = document.getElementById("demo");
 
-//2. cREA LA FUNCION QUE CAMBIA EL CONTENIDO
+// CREA LA FUNCION QUE CAMBIA EL CONTENIDO
 function cambiarTexto() {
     texto.innerHTML="Cambio automaticooooooooooooooo";
     boton.style.fontSize="40px"
